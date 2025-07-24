a6afa51ade963c938367ec66be59a8f2aa4e5ca0
Michael Granger <ged@FaerieMUD.org>
2025-07-24 10:50:28 -0700

    Switch doc generator to Sixfish

--- gem.deps.rb
+++ gem.deps.rb
@@ -6,6 +6,6 @@ group :development do
 	gem 'pg', '~> 1.2'
 	gem 'rake-deveiate', '~> 0.19'
 	gem 'simplecov', '~> 0.13'
-	gem 'rdoc-generator-fivefish', '~> 0.3'
+	gem 'rdoc-generator-sixfish', '~> 0.3'
 end
 
